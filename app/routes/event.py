3cd6ea15b761b1fe284800f66ccdb9ee86364848
Hussam Abubakr <hussam.sam12345@gmail.com>
2024-09-21 16:12:01 -0400

    Add Attendees Logic to get_event()

--- app/routes/event.py
+++ app/routes/event.py
@@ -44,7 +44,26 @@ def get_event(event_id):
         if location_response.data:
             response.data[0]['location'] = location_response.data[0]
             del response.data[0]['address_id']
-            return jsonify(response.data[0]), 200
+        attendee_response = supabase.table('EventRegistration').select('*').eq('event_id', event_id).execute()
+        attendees_user_info = []
+
+        if attendee_response.data:
+            for attendee in attendee_response.data:
+                user_id = attendee['user_id']
+                print(user_id)
+                user_response = supabase.table('users').select('*').eq('user_id', user_id).execute()
+                
+                if user_response.data:
+                    # Combine user info with attendee data
+                    attendee['user_info'] = user_response.data[0]
+
+                 # Add to the list of attendees
+                attendees_user_info.append(attendee)
+         # Add the attendees (with user info) to the event data
+        response.data[0]['attendees'] = attendees_user_info
+
+
+        return jsonify(response.data[0]), 200
     else:
         return jsonify({"error": "Event not found"}), 404
 
@@ -85,5 +104,8 @@ def cancel_event(event_id):
                 return jsonify({"error": "Event not found or could not be deleted"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+    
+
+
 
     
