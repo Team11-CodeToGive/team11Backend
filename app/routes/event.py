6b91c393192f25ab5abaa5219beeaea56a658c97
Hussam Abubakr <hussam.sam12345@gmail.com>
2024-09-21 13:56:57 -0400

    Add Location Logic

--- app/routes/event.py
+++ app/routes/event.py
@@ -9,6 +9,12 @@ def get_events():
     response = supabase.table('Events').select('*').execute()
 
     if response.data:
+        for i in range(len(response.data)):
+            location_response = supabase.table('Location').select('*').eq('id', response.data[i]['address_id']).execute()
+            if location_response.data:
+                response.data[0]['location'] = location_response.data[0]
+                del response.data[0]['address_id']
+
         return jsonify(response.data), 200
     else:
         return jsonify([]), 200
@@ -17,11 +23,15 @@ def get_events():
 def create_event():
     event_data = request.get_json()
     try:
-        response = supabase.table('Events').insert(event_data).execute()
-        if len(response.data) > 0:
-            return jsonify({"message": "Event created successfully!"}), 201
-        else:
-            return jsonify({"error": response.error_message}), 400
+        location_response = supabase.table('Location').insert(event_data['location']).execute()
+        if len(location_response.data) > 0:
+            del event_data['location']
+            event_data['address_id'] = location_response.data[0]['id']
+            response = supabase.table('Events').insert(event_data).execute()
+            if len(response.data) > 0:
+                return jsonify({"message": "Event created successfully!"}), 201
+            else:
+                return jsonify({"error": response.error_message}), 400
     except Exception as e:
         print(e)
         return jsonify({"error": str(e)}), 400
@@ -31,32 +41,50 @@ def get_event(event_id):
     response = supabase.table('Events').select('*').eq('event_id', event_id).execute()
     
     if response.data:
-        return jsonify(response.data[0]), 200
+        location_response = supabase.table('Location').select('*').eq('id', response.data[0]['address_id']).execute()
+        if location_response.data:
+            response.data[0]['location'] = location_response.data[0]
+            del response.data[0]['address_id']
+            return jsonify(response.data[0]), 200
     else:
         return jsonify({"error": "Event not found"}), 404
 
 @bp.route('/<int:event_id>', methods=['PUT'])
 def update_event(event_id):
-    data = request.get_json()
+    new_data = request.get_json()
 
-    if not data:
+    if not new_data:
         return jsonify({"error": "No data provided to update"}), 400
-    
-    response = supabase.table('Events').update(data).eq('event_id', event_id).execute()
 
-    if response.data:
-        return jsonify(response.data[0]), 200
-    else:
-        return jsonify({"error": "Event not found or no fields updated"}), 404
+    try:
+        location_response = supabase.table('Location').insert(new_data['location']).execute()
+        if len(location_response.data) > 0:
+            del new_data['location']
+            new_data['address_id'] = location_response.data[0]['id']
+            response = supabase.table('Events').update(new_data).eq('event_id', event_id).execute()
+
+            if response.data:
+                return jsonify(response.data[0]), 200
+            else:
+                return jsonify({"error": "Event not found or no fields updated"}), 404
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
     
 
 @bp.route('/<int:event_id>', methods=['DELETE'])
 def cancel_event(event_id):
-    response = supabase.table('Events').delete().eq('event_id', event_id).execute()
 
-    if response.data:
-        return jsonify(response.data[0]), 200
-    else:
-        return jsonify({"error": "Event not found or could not be deleted"}), 404
+    response = supabase.table('Events').delete().eq('event_id', event_id).execute()
+    try:
+        if response.data:
+            location_response = supabase.table('Location').delete().eq('id', response.data[0]['address_id']).execute()
+            if location_response:
+                return jsonify(response.data[0]), 200
+            else:
+                return jsonify({"error": "Event not found or could not be deleted"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
     
